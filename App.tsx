eff5b70a5310775916b18c236a5797da9ed1129b
matigoal <dugamin.mathias@gmail.com>
2020-08-02 17:22:49 +0200

    CRUD works

--- App.tsx
+++ App.tsx
@@ -4,11 +4,9 @@ import {
   TextInput,
   Button,
   StyleSheet,
-  // AsyncStorage,
   Text,
- 
 } from "react-native";
-import { IArticle } from './models/Article';
+import { IArticle } from "./models/Article";
 
 interface IState {
   article: IArticle;
@@ -16,37 +14,86 @@ interface IState {
 }
 
 // App main  component
-const App: React.FC = () => {
-  const [price, setPrice] = useState<string>('');
-  const [localization, setLocalization] = useState<string>('');
-  const [categorie, setCategorie] = useState<string>('');
-  const [image, setImage] = useState<string>('');
-  const [comment, setComment] = useState<string>('');
+const App: React.FC<IState> = () => {
+  const [price, setPrice] = useState<string>("");
+  const [localization, setLocalization] = useState<string>("");
+  const [categorie, setCategorie] = useState<string>("");
+  const [image, setImage] = useState<string>("");
+  const [comment, setComment] = useState<string>("");
+  const [id, setId] = useState<number>(0);
   let [articles, setArticles] = useState<Array<IArticle>>([]);
 
-  let id = 0;
+  // let id = 0;
 
   const submit = () => {
-    const newArticle: IArticle = { id: id++, price, localization, categorie, image, comment };
-    articles = [...articles, newArticle]
+    const newArticle: IArticle = {
+      id: id,
+      price,
+      localization,
+      categorie,
+      image,
+      comment,
+    };
+    setId(id + 1);
+    articles = [...articles, newArticle];
     setArticles(articles);
-    setPrice('');
-    setLocalization('');
-    setCategorie('');
-    setImage('');
-    setComment('');
+    setPrice("");
+    setLocalization("");
+    setCategorie("");
+    setImage("");
+    setComment("");
     console.log(articles);
-  }
+  };
 
   const handleDelete = (id: number) => {
-    articles = [...articles.filter(a => a.id !== id)]
-    setArticles(articles)
+    articles = [...articles.filter((a) => a.id !== id)];
+    setArticles(articles);
     console.log(articles);
-  }
+  };
 
-  const handleList = ()=> {
+  const handleEdit = (id: number) => {
+    console.log("fddd");
+    let i: number = 0;
+
+    let cpyArt = [...articles];
+
+    for (const line of cpyArt) {
+      if (line.id === id) {
+        cpyArt[i].price = price;
+        cpyArt[i].localization = localization;
+        cpyArt[i].categorie = categorie;
+        cpyArt[i].image = image;
+        cpyArt[i].comment = comment;
+      }
+      ++i;
+    }
+
     
-  }
+    setArticles(cpyArt);
+    console.log(articles);
+  };
+ 
+
+  const styles = StyleSheet.create({
+    container: {
+      paddingTop: 23,
+    },
+    input: {
+      margin: 15,
+      height: 40,
+      borderColor: "#FFF",
+      borderWidth: 4,
+    },
+    submitButton: {
+      backgroundColor: "#7a42f4",
+      padding: 10,
+      margin: 15,
+      height: 40,
+    },
+    submitButtonText: {
+      color: "white",
+    },
+  });
 
   return (
     <View style={styles.container}>
@@ -61,12 +108,11 @@ const App: React.FC = () => {
         <h1> Spend App</h1>
         <p style={{ marginTop: "5%" }}>Welcome to App for list your spend.</p>
         <div style={{ marginTop: "10%" }}>
-
           <TextInput
             placeholder="Price"
-            underlineColorAndroid='transparent'
+            underlineColorAndroid="transparent"
             style={styles.input}
-            keyboardType={'numeric'}
+            keyboardType={"numeric"}
             onChangeText={(text) => setPrice(text)}
             value={price}
           />
@@ -105,7 +151,6 @@ const App: React.FC = () => {
               setImage(text);
             }}
             value={image}
-
           />
           <TextInput
             style={styles.input}
@@ -118,10 +163,12 @@ const App: React.FC = () => {
             }}
             value={comment}
           />
-          <Button title='Submit' onPress={
-            () => { submit() }
-          } />
-
+          <Button
+            title="Submit"
+            onPress={() => {
+              submit();
+            }}
+          />
         </div>
 
         <div
@@ -131,62 +178,40 @@ const App: React.FC = () => {
             marginLeft: "auto",
             marginRight: "auto",
           }}
-        >
-          <Button
-            title="Edit"
-            color="#D6CD00"
-            accessibilityLabel="Learn more about this yellow button"
-            onPress={() => console.log('Edit')}
-          />
-          {/* <Button
-            title="Delete"
-            color="#d63600"
-            accessibilityLabel="Learn more about this red button"
-            onPress={() => {handleDelete()}
-          /> */}
-        </div>
+        ></div>
         <h1>List of Spend</h1>
 
         {articles.map((article) => {
-          return(
-          <View>
-           
-            <Text >{article.price}</Text>
-            <Text>{article.localization}</Text>
-            <Text>{article.categorie}</Text>
-            <Text>{article.image}</Text>
-            <Text>{article.comment}</Text>
-          </View>)
-          
+          return (
+            <View key={"art" + article.id}>
+              <Text>{article.price}</Text>
+              <Text>{article.localization}</Text>
+              <Text>{article.categorie}</Text>
+              <Text>{article.image}</Text>
+              <Text>{article.comment}</Text>
+              <Button
+                title="Edit"
+                color="#D6CD00"
+                accessibilityLabel="Learn more about this yellow button"
+                onPress={() => handleEdit(article.id)}
+              />
+              <Button
+                title="Delete"
+                color="#d63600"
+                accessibilityLabel="Learn more about this red button"
+                onPress={() => {
+                  handleDelete(article.id);
+                }}
+              />
+            </View>
+          );
         })}
 
-        <Button title='List of Spend' onPress={(e) => console.log('All')} />
-        <p id='lol'>
-        </p>
+        <Button title="List of Spend" onPress={(e) => console.log("All")} />
+        <p id="lol"></p>
       </div>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 23,
-  },
-  input: {
-    margin: 15,
-    height: 40,
-    borderColor: "#FFF",
-    borderWidth: 4,
-  },
-  submitButton: {
-    backgroundColor: "#7a42f4",
-    padding: 10,
-    margin: 15,
-    height: 40,
-  },
-  submitButtonText: {
-    color: "white",
-  },
-});
+};
 
 export default App;
--- models/Article.tsx
+++ models/Article.tsx
@@ -1,8 +1,9 @@
 export interface IArticle{
     id: number;
-    price: string,
-    localization: string,
-    categorie: string,
-    image: string,
-    comment: string,
+    price: string;
+    localization: string;
+    categorie: string;
+    image: string;
+    comment: string;
 }
+
